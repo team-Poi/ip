041447fca5a398fe98427d4031477f7249b69f52
Oein <oein0219@gmail.com>
2023-07-10 00:40:07 +0900

    to rewrite

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
+  async rewrites() {
     return [
       {
         source: "/:path*",
