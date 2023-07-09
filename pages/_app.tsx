cbde487330d115f9815a2fa43a4090a1a864f736
Oein <oein0219@gmail.com>
2023-07-10 00:42:59 +0900

    no imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,3 @@
-import "@/styles/globals.css";
-import "@team.poi/ui/src/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
